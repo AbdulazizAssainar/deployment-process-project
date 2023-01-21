a737b7d7be34e3ec8288a32c92961aca6c8479c3
AbdulazizAssainar <abdlaziz.a.abdullah@gmail.com>
2023-01-21 04:51:06 +0200

    commit

---
+++ udagram/set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=database-1.c1apqx5cutuh.us-east-2.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::elasticbeanstalk-us-east-2-836590063728
+export AWS_REGION=us-east-2
+export AWS_PROFILE=default
+export JWT_SECRET=mysecretstring
+export URL=http://localhost:8100
