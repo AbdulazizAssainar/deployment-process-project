5ef4b3f13031007ab3daad4381fb0cc4a02e498b
AbdulazizAssainar <abdlaziz.a.abdullah@gmail.com>
2023-01-22 12:05:50 +0200

    commit

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -2,7 +2,7 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=postgres
-export POSTGRES_HOST=database-1.c1apqx5cutuh.us-east-2.rds.amazonaws.com
+export POSTGRES_HOST=postgres://postgres:postgres@database-1.c1apqx5cutuh.us-east-2.rds.amazonaws.com:5432/postgres
 export POSTGRES_DB=postgres
 export AWS_BUCKET=arn:aws:s3:::udagramapi
 export AWS_REGION=us-east-2
